bb672f49c4c29dbb19d26301135e9ec8391c1025
Alexander Bauer <alexandrej.bauer@gmail.com>
2017-02-10 22:57:22 -0800

    Update Attack.h

--- src/Attack.h
+++ src/Attack.h
@@ -1,6 +1,14 @@
+// This class holds the implementation for an Attack obj.
+// Only successful attacks are passed to the queue.
+// When the attack reaches execution time, it performs.
+// This involves compromisig a certain computer and 
+// possibly triggering a notify event.
+
 class Attack : public Event
 {
 	private:
+	//Number of source. -1 for the attacker and
+	//integer for a computer
 		int sourceNum;
 
 	public:
@@ -17,12 +25,22 @@ class Attack : public Event
 		// ACCESSORS
 		int getSourceNum( ) const { return sourceNum; }
 
+	//This class also holds the method for IDS action.
+	//Only successful Notifications are sent to admin
 		static void IDSPerform( long long int currentTime,
 				        int           percentDetect,
 				        int           targetNum,
 				        int           sourceNum,
-				        MinBinHeap *& eventQueue    )
+				        int           numComputers,
+					MinBinHeap *& eventQueue    )
 		{
+		//Check if attaacker is attacking or if computers cross
+		//switches in tree.
+		   if(sourceNum == -1 || (targetNum <= numComputers/2
+		   		       && sourceNum > numComputers/2)
+				      || (targetNum > numComputers/2
+				       && sourceNum <= numComputers/2))
+		   {
 			if ( (rand()%100 + 1) <= percentDetect )
 			{
 				Event* newNote =
@@ -30,11 +48,12 @@ class Attack : public Event
 				             targetNum, sourceNum );
 				eventQueue->insert( newNote );
 			}
-
+                   }
 
 		}
 
-		// VIRTUAL FUNCTION OVERRIDE
+		// Perform function for attack. Compromise computer and
+		// trigger IDS possibility for notification.
 		virtual void perform( long long int            currentTime,
 				      long long int &          lastFixTime,
 				      int                      percentSuccess,
@@ -51,8 +70,8 @@ class Attack : public Event
 			}
 
 			IDSPerform( currentTime, percentDetect, targetNum,
-						 sourceNum,     eventQueue );
-
+				    sourceNum,   numComputers,  eventQueue );
+			//Output message
 			std::cout << "ATTACK: " << executionTime << ", "
 				  << sourceNum << ", " << targetNum << "\n";
 		}
